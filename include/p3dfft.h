f3d20445727245f8feb5934e0b9033f8f2927eca
dmitrypek <dmitrypek@98471b84-2354-11df-b9ed-85270ab41615>
2010-08-05 22:46:17 +0000

    Updated bits and ends

--- include/p3dfft.h
+++ include/p3dfft.h
@@ -1,25 +1,23 @@
 /*
 ! This file is part of P3DFFT library
 !
-! Version 2.3
+! Version 2.4
 !
-! Copyright (C) 2006-2008 Dmitry Pekurovsky
+! Copyright (C) 2006-2010 Dmitry Pekurovsky
+! Copyright (C) 2006-2010 UC San Diego
 !
-!    P3DFFT is free software; you can redistribute it and/or modify
-!    it under the terms of the GNU General Public License as published by
-!    the Free Software Foundation; either version 2 of the License, or
-!    (at your option) any later version.
-
-!    This program is distributed in the hope that it will be useful,
-!    but WITHOUT ANY WARRANTY; without even the implied warranty of
-!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-!    GNU General Public License for more details.
-
-!    You should have received a copy of the GNU General Public License
-!    along with this program; if not, write to the Free Software
-!    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-!----------------------------------------------------------------------------
+! P3DFFT is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+! P3DFFT is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!
+! GNU General Public License for more details.
+! You should have received a copy of the GNU General Public License
+! along with P3DFFT.  If not, see <http://www.gnu.org/licenses/>.
+!
 */
 
 #include <stdlib.h>
